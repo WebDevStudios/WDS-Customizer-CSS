a5117a506984ad53acb04e9dec84584d25d53207
Brad Parbs <brad@bradparbs.com>
2015-03-02 08:20:40 -0600

    Replace final instance of renaming plugin that was missed.

--- js/wds-customizer-css.js
+++ js/wds-customizer-css.js
@@ -1,7 +1,7 @@
 ( function( $ ) {
 	wp.customize( 'wds_custom_css', function( value ) {
 	value.bind( function( $new_styles ) {
-		$('#wds-custom-css').html( $new_styles );
+		$('#wds-customizer-css').html( $new_styles );
 	} );
 } );
 
